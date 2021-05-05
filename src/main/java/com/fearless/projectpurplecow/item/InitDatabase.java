a7096aa27c33f0c8e81c114ed2344a1ac28f9277
Randmness <mrand86@gmail.com>
2021-05-05 19:39:15 -0400

    Setting up database config

---
+++ src/main/java/com/fearless/projectpurplecow/item/InitDatabase.java
@@ -0,0 +1,19 @@
+package com.fearless.projectpurplecow.item;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class InitDatabase {
+    private static final Logger LOG = LoggerFactory.getLogger(InitDatabase.class);
+
+    @Bean
+    CommandLineRunner initializeDatabase(ItemRepository repository) {
+        return args -> {
+            LOG.info("Database started.");
+        };
+    }
+}
---
+++ src/main/java/com/fearless/projectpurplecow/item/Item.java
@@ -0,0 +1,33 @@
+package com.fearless.projectpurplecow.item;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+
+    private @Id @GeneratedValue Long id;
+    private String name;
+
+    Item() {};
+    Item(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
