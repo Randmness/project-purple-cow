ad92af9746ec7c0256422334034c52b708ed3314
Randmness <mrand86@gmail.com>
2021-05-05 18:45:32 -0400

    Initial commit of trial project

---
+++ src/main/java/com/fearless/projectpurplecow/ProjectPurpleCowApplication.java
@@ -0,0 +1,13 @@
+package com.fearless.projectpurplecow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPurpleCowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPurpleCowApplication.class, args);
+	}
+
+}
